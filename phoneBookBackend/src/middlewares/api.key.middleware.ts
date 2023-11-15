d3cece4253aa81f3bb9495d010c0d52f1f9183f6
Matheus Monteiro <monteiromatheus047@gmail.com>
2023-11-14 23:06:38 -0300

    update!: docker compose working
    project now runs using docker-compose up

--- phoneBookBackend/src/middlewares/api.key.middleware.ts
+++ phoneBookBackend/src/middlewares/api.key.middleware.ts
@@ -7,7 +7,7 @@ export class ApiKeyMiddleware implements NestMiddleware {
     use(req: Request, res: Response, next: NextFunction) {
         const apiKey = req.headers['api-key'];
         if (apiKey !== process.env.API_KEY) {
-            return res.status(401).json({ message: 'ApiKey inválida' });
+            return res.status(401).json({ message: 'Invalid api Key' });
         }
 
         next();
